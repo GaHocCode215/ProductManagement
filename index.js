629eeaf17413d08cc08ef28a93789f1fb177d0cc
GaHocCode215 <dominhit215@gmail.com>
2023-12-24 01:46:27 +0700

    create-admin-dashboard

--- index.js
+++ index.js
@@ -6,18 +6,20 @@ dotenv.config();
 
 databse.connect();
 
+const routesAdmin = require("./routes/admin/index.route");
 const routesClient = require("./routes/client/index.route");
 
 
 const app = express();
 const port = process.env.PORT;
 
-
-
 app.set("views", "./views");
 app.set("view engine", "pug");
 
 app.use(express.static("public"));
+
+//Routes Admin
+routesAdmin(app);
 
 // Routes Client
 routesClient(app);
