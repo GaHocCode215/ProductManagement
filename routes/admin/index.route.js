793909437bd26b25e57a234c6b6f28461ef8935a
GaHocCode215 <dominhit215@gmail.com>
2024-01-22 16:11:20 +0700

    add-account

--- routes/admin/index.route.js
+++ routes/admin/index.route.js
@@ -1,6 +1,7 @@
 const dashboardRoutes = require("./dashboard.route");
 const productRoutes = require("./product.route");
 const productCategoryRoutes = require("./product-category.route");
+const accountRoutes = require("./account.route");
 const systemConfig = require("../../config/system");
 const roleRoutes = require("./role.route");
 
@@ -12,4 +13,5 @@ module.exports = (app) => {
   app.use(`${PATH_ADMIN}/products`, productRoutes);
   app.use(`${PATH_ADMIN}/products-category`, productCategoryRoutes);
   app.use(`${PATH_ADMIN}/roles`, roleRoutes);
+  app.use(`${PATH_ADMIN}/accounts`, accountRoutes);
 }
