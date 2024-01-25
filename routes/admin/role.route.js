15d9e2dac922183d76450e98f4ed141a70f73a90
GaHocCode215 <dominhit215@gmail.com>
2024-01-25 13:12:47 +0700

    add-auth-middleware

--- routes/admin/role.route.js
+++ routes/admin/role.route.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 const controller = require("../../controllers/admin/role.controller");
 
+
 router.get("/", controller.index);
 
 router.get("/create", controller.create);
