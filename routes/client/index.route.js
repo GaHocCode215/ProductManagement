175c75bd5c7f6ca8c7fe1efad34b3568cf637cb0
GaHocCode215 <dominhit215@gmail.com>
2024-02-09 00:50:38 +0700

    add-cart

--- routes/client/index.route.js
+++ routes/client/index.route.js
@@ -2,10 +2,14 @@ const productRoutes = require("./products.route");
 const homeRoutes = require("./home.route");
 const categoryMiddleware = require("../../middlewares/client/category.middleware");
 const searchRoutes = require("./search.route");
+const cartRoutes = require("./cart.route");
+const cartMiddleware = require("../../middlewares/client/cart.middleware");
 
 module.exports = (app) => {
     app.use(categoryMiddleware.category);
+    app.use(cartMiddleware.cart);
     app.use("/", homeRoutes);
     app.use("/products", productRoutes);
     app.use("/search", searchRoutes);
+    app.use("/cart", cartRoutes);
 }
